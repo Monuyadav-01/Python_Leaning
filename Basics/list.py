bf1c3d553161d08f8c9ffe056900f212d70fdf94
monu <monuyadav10012000@gmail.com>
2024-05-23 00:41:32 +0530

    20 days python master

---
+++ Basics/list.py
@@ -0,0 +1,29 @@
+"""
+Lists are used to store multiple items in a single variable.
+
+Lists are one of 4 built-in data types in Python used to store collections of data, the other 3 are Tuple, Set, and Dictionary, all with different qualities and usage.
+
+Lists are created using square brackets:
+
+"""
+#Create a List:
+
+# list= ["fruits", "veg" , "nonveg"]
+# print(list)
+# print(list.index("veg"))
+# print(list.pop(1))
+# print(list)
+
+"""
+
+Changeable
+The list is changeable, meaning that we can change, add, and remove items in a list after it has been created.
+
+Allow Duplicates
+Since lists are indexed, lists can have items with the same value:
+"""
+
+list= ["fruits", "veg" , "nonveg" , "veg"]
+print(list)
+# print(list.index("veg"))
+# print(type(list))
--- Basics/string.py
+++ Basics/string.py
@@ -16,4 +16,15 @@ list= "Hello monu"
 # if "Monu" in list : print("Monu", "exist in list")
 # else : print("not exist")
 
-# hum java me substring bolte h use python me slicing kahte h so this 
+# hum java me substring bolte h use python me slicing kahte h 
+
+# print(list[2 : 5])
+
+age = 36
+# txt = "My name is John, I am " 
+# print(txt + age)  giving error for int not add in string
+
+# print(txt + str(age))  # convert int to string
+
+txt = f"My name is John, I am {age} " 
+print(txt)
